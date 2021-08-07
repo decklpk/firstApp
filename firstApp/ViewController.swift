1323798050654933ce3b8f7f332c69c9c0627958
Dmitry Khartuev <decklpk@gmail.com>
2021-08-07 19:01:03 +0300

    f

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Dmitry K on 07.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
